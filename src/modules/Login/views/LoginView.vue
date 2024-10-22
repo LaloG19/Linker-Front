845865d2bbccea5b63cb3a1561da68b0abd544f1
LaloG19 <21362134@utcancun.edu.mx>
2024-10-22 16:42:24 -0500

    hola gta 5

--- src/modules/Login/views/LoginView.vue
+++ src/modules/Login/views/LoginView.vue
@@ -1,14 +1,22 @@
 <template>
-    <p> Login View literalmente Firebase </p>
-  </template>
-  
-  <script setup lang="ts">
-  import { onMounted } from "vue";
-  import { useTitle } from "../../../composables/useTitle";
-  const { updateTitle } = useTitle("Mi Aplicación");
-  
-  onMounted(() => {
-    updateTitle("Login");
-  });
-  </script>
-  
+  <h1> Linker </h1>
+
+  <div class="">
+      <v-btn outlined color="white" class="mx-2" style="background-color: transparent; border-width: 2px; border-radius: 32px;">
+        Button 1
+      </v-btn>
+      <v-btn outlined color="white" class="mx-2" style="background-color: transparent; border-width: 2px; border-radius: 32px;">
+        Button 2
+      </v-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useTitle } from "../../../composables/useTitle";
+const { updateTitle } = useTitle("Mi Aplicación");
+
+onMounted(() => {
+  updateTitle("Login");
+});
+</script>
